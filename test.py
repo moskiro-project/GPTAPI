a2b8d00237e0be927b5978ce967bfafd4f0e6b11
maxel-up <57237409+maxel-up@users.noreply.github.com>
2023-07-16 13:23:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import openai
 import pandas as pd
 import time
-import csv
+import json
 openai.api_key = "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"
 
 excel_file = "./Data/2023_07_10_Datensatz_unbereinigt_LN_v3.xlsx"
@@ -12,7 +12,7 @@ outputJobSkills = []
 output = []
 
 #Generierung: Erste Anfrage
-for i in range(25,26):
+for i in range(2):
     content = ("Bereinige den folgenden Jobtitel von allen unnötigen Informationen:")
     content = content + data [i][1]
     responseTitle = openai.ChatCompletion.create(
@@ -26,31 +26,30 @@ for i in range(25,26):
 
     JobTitleResponse = responseTitle["choices"][0]["message"]["content"] # type: ignore
     time.sleep(1)
-    
-#Generierung: Zweite Anfrage
-    content1 = ("Identifiziere Skills in der Jobbeschreibung. Beschreibe die gefunden Skills mit 1-3 Schlagwörtern die genau in dieser Konstellation in dem Text zu finden sind und trenne sie mit Kommata. Ignoriere Vorteile im Unternehmen wie Urlaub oder Arbeitszeiten. Nutze dafür die folgende Jobbeschreibung:")
+
+    content1 = ("Identifiziere Skills in der Jobbeschreibung. Beschreibe die gefunden Skills mit 1-3 Schlagwörtern die genau in dieser Konstellation in dem Text zu finden sind, trenne sie mit Kommata und verwende keine Überschrift. Ignoriere Vorteile im Unternehmen wie Urlaub oder Arbeitszeiten. Nutze dafür die folgende Jobbeschreibung:")
+    #content1 = ("Verhalte dich als wärst du ein NER das Skills aus Texten erkennt. Erkenne alle angegebenen Skills in dem folgenden Text und gebe die Wörter in Form einer Liste wieder die durch Kommata getrennt wird. Wörter die das Arbeitsumfeld oder Arbeitsbedingungen betreffen sollen nicht ausgegeben werden: ")
     content1 = content1 + data [i][2]
     responseSkill = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[
-        #hier kann man dem system vor der Frage einige Vorgaben stellen
         {"role": "system", "content": "Du beantwortest Anfragen nur mit dem exakt gleichen Wortlaut wie sie in der Anfrage stehen. Aufzählungen mit Bindestrich dürfen nicht ausgeschrieben werden. Die Antwort soll ohne Einleitung und Überschrift erfolgen. Ignoriere Vorteile im Unternehmen wie Urlaub oder Arbeitszeiten."},
+        #{"role": "system", "content": "Arbeitsbedinungen und das Arbeitsumfeld sind keine Fähigkeiten die vom NER erfasst werden"},
         {"role": "user", "content": content1}
       ],
-      temperature = 0.7
+      temperature = 0.5
     )
 
     JobSkillResponse = responseSkill["choices"][0]["message"]["content"] # type: ignore
     output.append((JobTitleResponse,data[i][2],JobSkillResponse))
+    print(output[i][0],output[i][2])
     time.sleep(1)  
     
 
+df = pd.DataFrame(output, columns=["Jobtitle","Describtion","Jobskills"])
+print(df)
 
-print(output[0][2],output[0][0])
+with pd.ExcelWriter("example.xlsx") as writer:
+    df.to_excel(writer)
 
-with open("example.csv","w", newline="", encoding="utf-8-sig" ) as file:
-    writer = csv.writer(file, delimiter=";")
-
-    writer.writerow(["Jobtitle", "Decribtion", "Jobskill"])
-    for item in output:
-        writer.writerow(item)
+# with open("example.xlsx","w", newline="", encoding="utf-8-sig" ) as file:
